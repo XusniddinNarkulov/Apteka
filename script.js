086732a436ca7ce721ca7b5f52d004a995b792b9
XusniddinNarkulov <narkulof@gmail.com>
2022-03-27 17:39:13 +0500

    27 march 2022

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let lat;
+let long;
+
+navigator.geolocation.getCurrentPosition(function (e) {
+  lat = e.coords.latitude;
+  long = e.coords.longitude;
+
+  let map = L.map('map').setView([41.3398161, 69.2888034], 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker([41.3398161, 69.2888034, 17], {
+    opacity: 0.7,
+    draggable: true,
+  })
+    .addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+});
+setTimeout(function () {
+  console.log(lat, long);
+}, 1000);
