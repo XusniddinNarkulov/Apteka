28143d0b1b4a5e8f50c86c706b791e903b42f333
XusniddinNarkulov <narkulof@gmail.com>
2022-03-28 17:04:54 +0500

    28 march 2022

--- script.js
+++ script.js
@@ -13,26 +13,64 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let lat;
 let long;
+let map;
+let eventMap;
 
 navigator.geolocation.getCurrentPosition(function (e) {
   lat = e.coords.latitude;
   long = e.coords.longitude;
 
-  let map = L.map('map').setView([41.3398161, 69.2888034], 13);
+  map = L.map('map').setView([41.3398161, 69.2888034], 18);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
-  L.marker([41.3398161, 69.2888034, 17], {
+  map.on('click', function (e) {
+    // console.log(e);
+    eventMap = e;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  });
+});
+
+setTimeout(function () {
+  console.log(lat, long);
+}, 1000);
+
+console.log(L);
+
+form.addEventListener('submit', function (e) {
+  inputCadence.value =
+    inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+      '';
+
+  e.preventDefault();
+
+  L.marker([eventMap.latlng.lat, eventMap.latlng.lng], {
     opacity: 0.7,
     draggable: true,
   })
     .addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .bindPopup(
+      L.popup({
+        // autoPan: false,
+        // keepInView: true,
+        // closeButton: false,
+        maxWidth: 200,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      }).setContent('Hello World')
+    )
     .openPopup();
 });
-setTimeout(function () {
-  console.log(lat, long);
-}, 1000);
+
+inputType.addEventListener('change', function (e) {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
